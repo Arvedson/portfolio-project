d9b21065e36069190cce53d5ac6678b0ac5a2222
Arvedson <tomasarvedson@gmail.com>
2024-11-10 00:00:15 -0600

    star bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-"use client"; // Esto asegura que el archivo se ejecute en el cliente (Client Component)
+"use client";
 
 import "./globals.css";
 import Navbar from '../../src/app/components/Navbar';
 import Footer from "./components/Footer";
-import '/src/config/i18n'; // Importamos la configuración de i18n
-
+import '/src/config/i18n';
+// RootLayout.js
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,9 +13,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {/* Contenedor del fondo animado */}
+        <div className="background-container">
+          <div className="night">
+            {[...Array(7)].map((_, i) => {
+              // Genera valores aleatorios para cada estrella
+              const randomTop = Math.random() * 100;  // Valor entre 0% y 100% para top
+              const randomLeft = Math.random() * 100; // Valor entre 0% y 100% para left
+              const randomDelay = Math.random() * 5;  // Valor entre 0s y 5s para el retraso de la animación
+
+              return (
+                <div
+                  key={i}
+                  className="shooting_star"
+                  style={{
+                    top: `${randomTop}%`,
+                    left: `${randomLeft}%`,
+                    animationDelay: `${randomDelay}s`
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+        </div>
+
+        {/* Contenido principal de la aplicación */}
         <Navbar />
-        {children}
-        <Footer/>
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
